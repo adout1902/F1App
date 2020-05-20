1c4b92bcb2982159ba8aa5fa69e1538e44e452f0
adout1902 <50958594+adout1902@users.noreply.github.com>
2020-05-20 23:55:51 +0200

    oops, changed to placeholders for uname and password

--- F1App.py
+++ F1App.py
@@ -1,7 +1,7 @@
 import pymongo
 
 
-client = pymongo.MongoClient("mongodb+srv://aaisha:aaisha1902@clusterf1-gof0m.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://<username>:<password>@clusterf1-gof0m.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.test
 
 for x in client.f1.drivers.find():  
