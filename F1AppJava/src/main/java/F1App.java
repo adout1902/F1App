c980c90cf7ca350b3b87fe870ba6564b2ee369d3
adout1902 <50958594+adout1902@users.noreply.github.com>
2020-05-20 23:46:57 +0200

    Java

---
+++ F1AppJava/src/main/java/F1App.java
@@ -0,0 +1,35 @@
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClientSettings;
+
+
+public class F1App
+{
+
+    public static void main(String[] args)
+    {
+        ConnectionString connString = new ConnectionString(
+                "mongodb+srv://aaisha:aaisha1902@clusterf1-gof0m.gcp.mongodb.net/test?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .retryWrites(true)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("f1");
+
+
+
+        MongoCollection<Document> drivers = database.getCollection("drivers");
+
+        // find one document with new Document
+        Document student1 = drivers.find(new Document("_id", 1)).first();
+        System.out.println("Driver 1: " + student1.toJson());
+
+
+    }
+}
